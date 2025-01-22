f18d539ce13bd8792153cb79669d223db2412111
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-01-22 14:40:34 +0100

    style: fixed format

--- crates/gravitron_renderer/src/ecs/systems/mod.rs
+++ crates/gravitron_renderer/src/ecs/systems/mod.rs
@@ -1,2 +1,2 @@
-pub mod renderer;
 pub mod propagation;
+pub mod renderer;
--- crates/gravitron_renderer/src/ecs/systems/propagation.rs
+++ crates/gravitron_renderer/src/ecs/systems/propagation.rs
@@ -3,6 +3,9 @@ use gravitron_hierarchy::propagation::UpdatePropagationQuery;
 
 use crate::ecs::components::transform::{GlobalTransform, Transform};
 
-pub fn transform_propagate(query: UpdatePropagationQuery<Transform, GlobalTransform>, cmds: &mut Commands) {
+pub fn transform_propagate(
+  query: UpdatePropagationQuery<Transform, GlobalTransform>,
+  cmds: &mut Commands,
+) {
   query.propagate(cmds);
 }
